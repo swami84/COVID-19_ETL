0168ae4ff48d8a28114435ee54e3f7a4d0ccc869
Swami <44759539+swami84@users.noreply.github.com>
2020-03-30 18:37:34 -0400

    ETL:added activate script to active the environment

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
+#!/bin/bash
 echo $"Installing the dependencies for Covid-19 ETL Task"
 python3 -m venv env
-alias activate_env = "source /env/bin/activate"
-activate_env
+chmod u+x ./activate.sh
+source ./activate.sh
 cd env
 cp ../requirements.txt req.txt
 pip install -r req.txt
