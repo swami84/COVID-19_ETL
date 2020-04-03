1f7cb0db21fb29e6f9b37667990be1212344b260
Swami <44759539+swami84@users.noreply.github.com>
2020-04-03 01:44:59 -0400

    Added death data, and column filters

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 echo $"Installing the dependencies for Covid-19 ETL Task"
 python3 -m venv env
 chmod u+x ./activate.sh
-source ($pwd)/activate.sh
+source $(pwd)/activate.sh
 cd env
 cp ../requirements.txt req.txt
 pip install -r req.txt
