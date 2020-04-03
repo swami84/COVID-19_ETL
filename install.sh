f3e7b0737a1dd6a13cec63789817734e4f62db91
Swami <44759539+swami84@users.noreply.github.com>
2020-04-03 02:22:52 -0400

    Added death data, and column filters

--- install.sh
+++ install.sh
@@ -6,16 +6,8 @@ python3 -m venv env
 #source $(pwd)/activate.sh
 #sourceFile="./env/bin/activate"
 #source ${sourceFile}
-activate () {
-    echo Activating Virtual Environment...
-    source $(pwd)/env/bin/activate
-}
-export -f activate
-activate
-source ~/.bash_profile
+. env/bin/activate
 
-# source the virtualenv
-source env/bin/activate
 cd env
 cp ../requirements.txt req.txt
 pip install -r req.txt
